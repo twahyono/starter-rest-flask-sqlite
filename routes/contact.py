662c942fabd7499986118c5f93e142a2e4e9b2ae
twahyono <teddy@qnptech.com>
2024-12-13 04:43:57 +0700

    add pagination

--- routes/contact.py
+++ routes/contact.py
@@ -9,12 +9,20 @@ bp = Blueprint("contact", __name__)
 @bp.get("/contact")
 def get_contact():
     try:
+        limit = request.args.get("limit")
+        offset = request.args.get("offset")
+        if limit is None:
+            limit=10
+        if offset is None:
+            offset=0
         con = get_db()
         cur = con.cursor()
-        contact = cur.execute("select name,email,createdAt from contact").fetchall()
+        contact = cur.execute(
+            "select name,email,createdAt from contact limit ? offset ?", (limit, offset)
+        ).fetchall()
         columns = [col[0] for col in cur.description]
         data = [dict(zip(columns, row)) for row in contact]
-        to_json = json.dumps(data, indent=2,default=str)
+        to_json = json.dumps(data, indent=2, default=str)
         return to_json
 
     except sqlite3.Error as error:
@@ -27,9 +35,9 @@ def get_contact_by_id(id):
     cur = con.cursor()
 
     contact = cur.execute("select * from contact where id=?", (id,)).fetchone()
-    columns = ["id","name","email","phone"]
-    data = dict(zip(columns, contact)) 
-    return json.dumps(data,default=str)
+    columns = ["id", "name", "email", "phone"]
+    data = dict(zip(columns, contact))
+    return json.dumps(data, default=str)
 
 
 @bp.post("/contact")
@@ -47,25 +55,24 @@ def create_contact():
         ),
     )
     con.commit()
-    columns = ["id","name","email","phone"]
-    data = dict(zip(columns, contact)) 
-    return json.dumps(data,default=str)
+    columns = ["id", "name", "email", "phone"]
+    data = dict(zip(columns, contact))
+    return json.dumps(data, default=str)
 
 
 @bp.put("/contact/<int:id>")
 def update_contact_by_id(id):
     updateContact = request.get_json()
     con = get_db()
-    
-    contact = con.execute("update contact set name=?, email=? phone=? where id=?", 
-                          (updateContact["name"],
-                           updateContact["email"],
-                           updateContact["phone"],
-                           id))
+
+    contact = con.execute(
+        "update contact set name=?, email=? phone=? where id=?",
+        (updateContact["name"], updateContact["email"], updateContact["phone"], id),
+    )
     con.commit()
-    columns = ["id","name","email","phone"]
-    data = dict(zip(columns, contact)) 
-    return json.dumps(data,default=str)
+    columns = ["id", "name", "email", "phone"]
+    data = dict(zip(columns, contact))
+    return json.dumps(data, default=str)
 
 
 @bp.delete("/contact/<int:id>")
@@ -73,4 +80,4 @@ def delete_contact_by_id(id):
     con = get_db()
     con.execute("delete from contact where id=?", (id,))
     con.commit()
-    return {"message":f"contact {id} is deleted."}
+    return {"message": f"contact {id} is deleted."}
